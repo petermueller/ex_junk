4f3940d1368e9ec79a4c1bb93a002d8131137a62
Peter Mueller <pmueller@covermymeds.com>
2017-04-26 17:00:38 -0400

    Actually include some description, scope to test only

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Junk
-
-**TODO: Add description**
+Junk is a module for generating Junk data in your tests.
+For when you don't care about the content, just that it has the right 'shape'.
+It is inspired by Dave Brady's [rspec-junklet](https://github.com/dbrady/rspec-junklet)
 
 ## Installation
 
@@ -9,11 +10,27 @@ by adding `ex_junk` to your list of dependencies in `mix.exs`:
 
 ```elixir
 def deps do
-  [{:ex_junk, "~> 0.1.0"}]
+  [{:ex_junk, "~> 0.1.0", only: :test}]
 end
 ```
 
-Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
-and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
-be found at [https://hexdocs.pm/ex_junk](https://hexdocs.pm/ex_junk).
+## Usage
+
+```elixir
+defmodule ExampleTest do
+  import Junk 
+
+  test "example test" do
+    thing = %SomeThing{
+      name:    junk, # defaults to junk(String)
+      some_id: junk(Integer, length: 9)
+    }
+    struct(SomeThing, thing)
+    |> SomeThing.push
+
+    assert SomeThing.pop == thing
+  end
+end
+```
+
 
--- mix.exs
+++ mix.exs
@@ -1,13 +1,23 @@
 defmodule Junk.Mixfile do
   use Mix.Project
+  @version "0.1.0"
 
   def project do
     [app: :ex_junk,
-     version: "0.1.0",
-     elixir: "~> 1.4",
+     version: @version,
+     elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps()]
+     deps: deps(),
+     description: description(),
+     package: package(),
+     docs: [
+       extras: ["README.md"],
+       main: "readme",
+       source_ref: "v#{@version}",
+       source_url: "https://github.com/felix-starman/ex_junk",
+     ]
+   ]
   end
 
   # Configuration for the OTP application
@@ -29,5 +39,22 @@ defmodule Junk.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [{:double, "~>0.4", only: :test}]
+  end
+
+  defp description do
+    """
+    Junk is a module for generating Junk data in your tests.
+    For when you don't care about the content, just that it has the right 'shape'.
+    """
+  end
+
+  defp package do
+    [# These are the default files included in the package
+     name: :ex_junk,
+     files: ["lib", "mix.exs", "README*", "LICENSE*"],
+     maintainers: ["Peter Mueller"],
+     licenses: ["MIT"],
+     links: %{"GitHub" => "https://github.com/felix-starman/ex_junk",
+      "Docs" => "https://github.com/felix-starman/ex_junk"}]
   end
 end
