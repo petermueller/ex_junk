8344f20e42de94b1393601fda60ad45f1cefe726
Owen Winkler <owinkler@covermymeds.com>
2017-04-28 17:23:05 -0400

    Refactor junk_for_type to use pattern matching instead

--- lib/ex_junk.ex
+++ lib/ex_junk.ex
@@ -4,49 +4,35 @@ defmodule Junk do
   It is inspired by Dave Brady's [rspec-junklet](https://github.com/dbrady/rspec-junklet)
   """
 
-  @inject %{random_bytes: &:crypto.strong_rand_bytes/1}
-
-  @default_opts [
-    byte_size: 32,
+  defstruct byte_size: 32,
     prefix: "",
-    rand_mod: @inject,
+    rand_mod: &:crypto.strong_rand_bytes/1,
     size: 16
-  ]
 
   @doc """
-  Returns junk(String).
+  Takes a Module (String, Integer) and options, returns junk for that Module.
   """
-  def junk, do: junk(String, @default_opts)
+  def junk(_, opts \\ [])
 
-  @doc """
-  Returns junk(String, opts)
-  """
-  def junk(opts) when is_list(opts) do
+  def junk(opts, _) when is_list(opts) do
     junk(String, opts)
   end
 
-  @doc """
-  Takes a Module (String, Integer), and options, returns junk for that Module.
-  """
-  def junk(junk_type, opts \\ @default_opts) do
-    merged_opts = @default_opts
-    |> Keyword.merge(opts)
-    |> Enum.into(%{})
-    junk_for_type(junk_type, merged_opts)
-  end
-
-  defp junk_for_type(:"Elixir.String", opts) do
-    string = random_bytes(opts)
+  def junk(String, opts) do
+    opts = construct_opts(opts)
+    string = opts.rand_mod.(opts.byte_size)
     |> Base.url_encode64
     opts.prefix <> "-" <> string
   end
-  defp junk_for_type(:"Elixir.Integer", opts) do
+
+  def junk(Integer, opts) do
+    opts = construct_opts(opts)
     min = :math.pow(10, opts.size-1) |> trunc
     max = :math.pow(10, opts.size)-1 |> trunc
     Range.new(min,max) |> Enum.random
   end
 
-  defp random_bytes(%{rand_mod: rand_mod, byte_size: b_size}) do
-    rand_mod.random_bytes.(b_size)
+  defp construct_opts(opts) do
+    Enum.into(opts, Map.from_struct(%Junk{}))
   end
 end
