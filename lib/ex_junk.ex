f691b0b159a940e8dde0cc46da473869ccfdb356
Owen Winkler <owinkler@covermymeds.com>
2017-04-29 17:29:47 -0400

    Use generator functions to supply random values.
    This allows values to be filtered using Stream.

--- lib/ex_junk.ex
+++ lib/ex_junk.ex
@@ -4,12 +4,15 @@ defmodule Junk do
   It is inspired by Dave Brady's [rspec-junklet](https://github.com/dbrady/rspec-junklet)
   """
 
+  @chars "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ" |> String.split("", trim: true)
+
   defstruct byte_size: 32,
     prefix: false,
-    rand_mod: &:crypto.strong_rand_bytes/1,
+    rand_mod: &Enum.random/1,
     size: 16,
     presets: Application.get_all_env(:ex_junk),
-    parameters: []
+    parameters: [],
+    unique: false
 
   @doc """
   Takes a Module (String, Integer) and options, returns junk for that Module.
@@ -21,23 +24,17 @@ defmodule Junk do
   end
 
   def junk(String, opts) do
-    opts = construct_opts(opts)
-    string = opts.rand_mod.(opts.byte_size)
-    |> Base.url_encode64
-    post_op(string, opts)
+    pick_one(&generate_string/1, opts)
   end
 
   def junk(Integer, opts) do
-    opts = construct_opts(opts)
-    min = :math.pow(10, opts.size-1) |> trunc
-    max = :math.pow(10, opts.size)-1 |> trunc
-    output = Range.new(min,max) |> Enum.random
-    post_op(output, opts)
+    pick_one(&generate_integer/1, opts)
   end
 
   def junk(f, opts) when is_function(f) do
-    opts = construct_opts(opts)
-    apply(f, opts.parameters)
+    pick_one(fn(opts) -> 
+      apply(f, opts.parameters)
+    end, opts)
   end
 
   def junk(preset_name, opts) when is_atom(preset_name) do
@@ -45,8 +42,29 @@ defmodule Junk do
     case get_in(opts, [:presets, preset_name]) do
       f when is_function(f) -> Junk.junk(f, opts)
       params when is_list(params) -> apply(Junk, :junk, params)
-      _ -> Junk.junk(String, prefix: Atom.to_string(preset_name))
+      _ -> Junk.junk(String, %{opts | prefix: Atom.to_string(preset_name)})
     end
+  end
+
+  defp pick_one(generator, opts) do
+    opts = construct_opts(opts)
+    Stream.repeatedly(fn -> generator.(opts) end)
+    |> Stream.map(&post_op(&1, opts))
+    |> Stream.filter(&filter(&1, opts))
+    |> Enum.take(1)
+    |> List.first
+  end
+
+  def generate_string(opts) do
+    Enum.reduce(1..opts.byte_size, [], fn(_i, acc) -> [opts.rand_mod.(@chars) | acc] end)
+    |> Enum.join("")
+  end
+
+  defp generate_integer(opts) do
+    min = :math.pow(10, opts.size-1) |> trunc
+    max = :math.pow(10, opts.size)-1 |> trunc
+    Range.new(min, max) 
+    |> Enum.random
   end
 
   defp construct_opts(opts) do
@@ -54,6 +72,18 @@ defmodule Junk do
   end
 
   defp post_op(output, opts) do
-    output = if opts.prefix, do: "#{opts.prefix}-#{output}", else: output
+    if opts.prefix, do: "#{opts.prefix}-#{output}", else: output
   end
+
+  defp filter(value, opts) do
+    if opts.unique, do: unique?(value), else: true
+  end
+
+  defp unique?(value) do
+    if :ets.info(:used_junk) == :undefined do
+      :ets.new(:used_junk, [:named_table])
+    end
+    :ets.insert_new(:used_junk, {value})
+  end
+
 end
